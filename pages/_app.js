630205dc6bfc355c91f617a905a66d25c8078664
pouriya-chibaie-dev <85185561+pouriya-chibaie-dev@users.noreply.github.com>
2022-09-08 09:33:54 +0430

    add file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
-
+import '../styles/font.css'
+import {useState} from 'react'
+import { Provider } from '../context/context'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<Provider>
+  <Component {...pageProps} />
+
+  </Provider>
 }
 
 export default MyApp
